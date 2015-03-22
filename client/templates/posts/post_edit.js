a0abef074a4bba08eb399228bbc4063659603719
dovadi <frank.oxener@gmail.com>
2015-03-22 12:05:32 +0100

    Up to and including 8 Editing posts

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,34 @@
+Template.postEdit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var currentPostId = this._id;
+
+    var postProperties = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    }
+
+    //Here we directly update the collection on the client side whereas
+    //for the creation of a Post we use a Server Method. See chapter 8 on Method Calls vs Client-side Data Manipulation
+    //See https://www.discovermeteor.com/blog/meteor-methods-client-side-operations/
+    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      if (error) {
+        // display the error to the user
+        alert(error.reason);
+      } else {
+        Router.go('postPage', {_id: currentPostId});
+      }
+    });
+  },
+
+  'click .delete': function(e) {
+    e.preventDefault();
+
+    if (confirm("Delete this post?")) {
+      var currentPostId = this._id;
+      Posts.remove(currentPostId);
+      Router.go('postsList');
+    }
+  }
+});
