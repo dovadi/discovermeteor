49eb332784a4621fb54534851991c2f8ed39ad57
dovadi <frank.oxener@gmail.com>
2015-03-23 20:38:56 +0100

    Up to and including 9: Errors

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,15 @@
+Template.postEdit.created = function() {
+  Session.set('postEditErrors', {});
+}
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postEdit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -9,13 +21,18 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val()
     }
 
+    var errors = validatePost(postProperties);
+      if (errors.title || errors.url)
+        return Session.set('postEditErrors', errors);
+
+
     //Here we directly update the collection on the client side whereas
     //for the creation of a Post we use a Server Method. See chapter 8 on Method Calls vs Client-side Data Manipulation
     //See https://www.discovermeteor.com/blog/meteor-methods-client-side-operations/
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
         // display the error to the user
-        alert(error.reason);
+        throwError(error.reason);
       } else {
         Router.go('postPage', {_id: currentPostId});
       }
