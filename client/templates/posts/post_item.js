1b0491da5fa8c3354e6b8212534da3884e7cead4
dovadi <frank.oxener@gmail.com>
2015-03-25 21:18:55 +0100

    Up to and including 10 Comments

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -7,9 +7,6 @@ Template.postItem.helpers({
 
   ownPost: function() {
     return this.userId === Meteor.userId();
-  },
-
-  commentsCount: function() {
-    return Comments.find({postId: this._id}).count();
   }
+
 });
